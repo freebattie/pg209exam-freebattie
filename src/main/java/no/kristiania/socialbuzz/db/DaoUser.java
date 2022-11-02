bd692f9855da89b7183393ed52669d668c9f6d64
Snorre <snle001@student.kristiania.no>
2022-11-02 19:54:40 +0100

    🟢 getAllUserLoginTest() (but forgot to use dao...)

--- src/main/java/no/kristiania/socialbuzz/db/DaoUser.java
+++ src/main/java/no/kristiania/socialbuzz/db/DaoUser.java
@@ -3,6 +3,8 @@ package no.kristiania.socialbuzz.db;
 import jakarta.inject.Inject;
 
 import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -16,9 +18,24 @@ public class DaoUser {
     }
 
 //    Used for getting the necessary info at login
-    public Map<Integer, String> getAllUserLogin() {
+    public Map<Integer, String> getAllUserLogin() throws SQLException {
 
-        return null;
+        var sql = """
+                SELECT id_user, username
+                FROM users;
+                """;
+
+        try (var statement = connection.prepareStatement(sql)) {
+            Map<Integer, String> users = new HashMap<>();
+            var result = statement.executeQuery();
+
+            while (result.next()) {
+                users.put(result.getInt(1), result.getString(2));
+            }
+
+            return users;
+        }
+
     }
 
     public List<User> getAllUsers() {
