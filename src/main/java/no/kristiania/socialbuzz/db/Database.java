c23ba6fc0459407726297af93bbec1153d56ee81
bjarte <22171847+freebattie@users.noreply.github.com>
2022-11-09 19:24:31 +0100

    database

--- src/main/java/no/kristiania/socialbuzz/db/Database.java
+++ src/main/java/no/kristiania/socialbuzz/db/Database.java
@@ -15,25 +15,19 @@ public class Database {
         var properties = new Properties();
         var dataSource = new HikariDataSource();
 
-//        if we upload with a .properties use that setting. otherwise use azure environment variables
-        if (Files.exists(Path.of("application.properties"))){
-            FileReader reader = new FileReader("application.properties");
-                try (reader) {
-                properties.load(reader);
+        FileReader reader = new FileReader("application.properties");
+        try (reader) {
+            properties.load(reader);
 
-                dataSource.setJdbcUrl(properties.getProperty("jdbc.url"));
-                dataSource.setUsername(properties.getProperty("jdbc.username"));
-                dataSource.setPassword(properties.getProperty("jdbc.password"));
-            }
+            dataSource.setJdbcUrl(properties.getProperty("jdbc.url"));
+            dataSource.setUsername(properties.getProperty("jdbc.username"));
+            dataSource.setPassword(properties.getProperty("jdbc.password"));
         }
-        else{
-            dataSource.setJdbcUrl(System.getenv("DB_URL"));
-            dataSource.setUsername(System.getenv("DB_USER"));
-            dataSource.setPassword(System.getenv("DB_PASSWORD"));
-        }
-
         var flyway = Flyway.configure().dataSource(dataSource).load();
         flyway.migrate();
+//        dataSource.setJdbcUrl(System.getenv("DB_URL"));
+//        dataSource.setUsername(System.getenv("DB_USER"));
+//        dataSource.setPassword(System.getenv("DB_PASSWORD"));
         return dataSource;
     }
 }
