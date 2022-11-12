0b20c66aa8dfcd2eeb6ad4a96883dfd510d59f91
bjarte <22171847+freebattie@users.noreply.github.com>
2022-11-12 19:13:35 +0100

    testing

--- src/main/java/no/kristiania/socialbuzz/Program.java
+++ src/main/java/no/kristiania/socialbuzz/Program.java
@@ -12,6 +12,7 @@ public class Program {
         int port = Optional.ofNullable(System.getenv("HTTP_PLATFORM_PORT"))
                 .map(Integer::parseInt)
                 .orElse(8080);
+
         new SocialBuzzServer(port, dataSource).start();
     }
 
