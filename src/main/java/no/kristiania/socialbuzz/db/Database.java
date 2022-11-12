b044be78e6eea804218a86fdae9377ad6b76d396
bjarte <22171847+freebattie@users.noreply.github.com>
2022-11-12 19:26:59 +0100

    fixing up database class

--- src/main/java/no/kristiania/socialbuzz/db/Database.java
+++ src/main/java/no/kristiania/socialbuzz/db/Database.java
@@ -12,23 +12,27 @@ public class Database {
     public static HikariDataSource getDataSource() throws IOException {
         var properties = new Properties();
         var dataSource = new HikariDataSource();
-
+        dataSource.setAutoCommit(false);
         FileReader reader = new FileReader("application.properties");
-        try (reader) {
+        if(reader != null) {
             properties.load(reader);
-            dataSource.setAutoCommit(false);
+
+            dataSource.setJdbcUrl(properties.getProperty("jdbc.url"));
+            dataSource.setUsername(properties.getProperty("jdbc.username"));
+            dataSource.setPassword(properties.getProperty("jdbc.password"));
+
+
+        }
+        else{
             dataSource.setJdbcUrl(System.getenv("DB_URL"));
             dataSource.setUsername(System.getenv("DB_USER"));
             dataSource.setPassword(System.getenv("DB_PASSWORD"));
-
         }
         var flyway = Flyway.configure().dataSource(dataSource).load();
         flyway.migrate();
 
 /*
-            dataSource.setJdbcUrl(properties.getProperty("jdbc.url"));
-            dataSource.setUsername(properties.getProperty("jdbc.username"));
-            dataSource.setPassword(properties.getProperty("jdbc.password"));
+
 */
         return dataSource;
     }
