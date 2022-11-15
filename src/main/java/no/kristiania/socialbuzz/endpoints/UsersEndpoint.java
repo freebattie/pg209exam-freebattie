cb0d51900be905b22212df8cfdaf7d22af8a44e3
Snorre <snle001@student.kristiania.no>
2022-11-15 16:56:03 +0100

    Added PUT test

--- src/main/java/no/kristiania/socialbuzz/endpoints/UsersEndpoint.java
+++ src/main/java/no/kristiania/socialbuzz/endpoints/UsersEndpoint.java
@@ -4,9 +4,8 @@ import com.google.gson.Gson;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
-import no.kristiania.socialbuzz.dao.Email;
-import no.kristiania.socialbuzz.dao.User;
 import no.kristiania.socialbuzz.dao.DaoUser;
+import no.kristiania.socialbuzz.dto.User;
 
 import java.sql.SQLException;
 import java.util.List;
@@ -26,20 +25,19 @@ public class UsersEndpoint {
 
     @PUT
     @Consumes(MediaType.APPLICATION_JSON)
-    public void editUser(String user) throws SQLException {
+    public void editUser(String userJson) throws SQLException {
         Gson gson = new Gson();
-
-        var test = gson.fromJson(user,User.class);
-
-        daoUser.EditUser(test);
+        var user = gson.fromJson(userJson,User.class);
+        daoUser.EditUser(user);
     }
 
     @GET
     @Path("/{id}")
     @Produces(MediaType.APPLICATION_JSON)
     public User getUserById(@PathParam("id") Long id) throws SQLException {
-        return daoUser.getUser(id);
+        return daoUser.getUserById(id);
     }
+
     @GET
     @Path("/emails")
     @Consumes(MediaType.APPLICATION_JSON)
@@ -47,17 +45,19 @@ public class UsersEndpoint {
     public long getEmailsByUserId(@QueryParam("id") Long id) throws SQLException {
         return daoUser.getLastEmailId(id);
     }
+
     @POST
     @Path("/emails")
     @Consumes(MediaType.APPLICATION_JSON)
-    public void AddUserByEmail(User user) throws SQLException {
+    public void addUserByEmail(User user) throws SQLException {
          daoUser.addEmail(user.getId_user());
     }
+
     @DELETE
     @Consumes(MediaType.APPLICATION_JSON)
     @Path("/emails/{id}")
     public void deleteEmailByID(@PathParam("id")long id) throws SQLException {
-        daoUser.removeEmail(id);;
+        daoUser.removeEmail(id);
     }
 
 }
--- src/test/java/no/kristiania/socialbuzz/endpoints/UserEndpointTest.java
+++ src/test/java/no/kristiania/socialbuzz/endpoints/UserEndpointTest.java
@@ -1,8 +1,10 @@
 package no.kristiania.socialbuzz.endpoints;
 
+import com.google.gson.Gson;
 import no.kristiania.socialbuzz.SocialBuzzServer;
 import no.kristiania.socialbuzz.dao.DaoUser;
 import no.kristiania.socialbuzz.db.InMemoryDataSource;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -10,6 +12,7 @@ import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.sql.Connection;
 import java.sql.SQLException;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -17,20 +20,25 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class UserEndpointTest {
 
     private SocialBuzzServer server;
+    private Connection connection;
     private DaoUser daoUser;
 
     @BeforeEach
-    public void h2DbSetup() throws SQLException {
-        var con = InMemoryDataSource.createTestDataSource();
-        this.daoUser = new DaoUser(con.getConnection());
-    }
-
-    @BeforeEach
-    public void setupServer() throws Exception {
-        this.server = new SocialBuzzServer(0, InMemoryDataSource.createTestDataSource());
+    public void h2DbSetup() throws Exception {
+        var dataSource = InMemoryDataSource.createTestDataSource();
+        this.server = new SocialBuzzServer(0, dataSource);
+        this.connection = dataSource.getConnection();
         server.start();
+
+        connection.setAutoCommit(false);
+        this.daoUser = new DaoUser(connection);
     }
 
+
+    @AfterEach
+    public void rollbackTest() throws SQLException {
+        connection.rollback();
+    }
 
     @Test
     public void getAllUsersTest() throws IOException, SQLException {
@@ -70,13 +78,31 @@ public class UserEndpointTest {
 
     @Test
     public void editUserTest() throws SQLException, IOException {
-        var user = daoUser.getUserById(1);
+        var user = daoUser.getUserById(4);
         user.setUsername("TestUser");
         user.setName("Ola Nordmann");
         user.setTlf("99887766");
-        daoUser.EditUser(user);
 
-        var connection = openConnection("/api/users/1");
+//        Make Json file out of user
+        var gson = new Gson();
+        var userJson = gson.toJson(user);
+
+//        Send changes to server
+        var preConnection = openConnection("/api/users");
+        preConnection.setRequestMethod("PUT");
+        preConnection.setRequestProperty("Content-Type", "application/json");
+        preConnection.setDoOutput(true);
+        preConnection.getOutputStream().write(
+                userJson.getBytes(StandardCharsets.UTF_8)
+        );
+
+//        Confirms that PUT executed
+        assertThat(preConnection.getResponseCode())
+                .as(preConnection.getResponseMessage())
+                .isEqualTo(204);
+
+//        Get edited user back and compare
+        var connection = openConnection("/api/users/4");
         connection.setRequestMethod("GET");
 
         assertThat(connection.getResponseCode())
@@ -90,7 +116,6 @@ public class UserEndpointTest {
                 .contains(user.getUsername())
                 .contains(user.getName());
     }
-
 
     private HttpURLConnection openConnection(String spec) throws IOException {
         return (HttpURLConnection) new URL(server.getURL(), spec).openConnection();
