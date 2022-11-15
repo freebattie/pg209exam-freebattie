8969431508a28eb013147409a1606c48efce74d2
bjarte <22171847+freebattie@users.noreply.github.com>
2022-11-15 16:19:19 +0100

    react update

--- src/main/client/src/App.jsx
+++ src/main/client/src/App.jsx
@@ -7,64 +7,13 @@ import {HashRouter, Route, Router, Routes, useNavigate} from "react-router-dom";
 import {createHashHistory} from 'history';
 import {useEffect, useState} from "react";
 import {EditUser} from "./edituser.jsx";
+import {NewChat} from "./newchat.jsx";
 
 
 const history = createHashHistory();
 
 
-function NewChat({users,activeUserId}) {
-    const [title, setTitle] = useState("");
-    const [chatters, setChatters] = useState([email]);
 
-    async function handelOnSubmit(){
-        await fetch("/api/chats", {
-            method: "POST",
-            body: JSON.stringify({title,chatters}),
-            headers: {
-                "Content-Type": "application/json"
-            }
-        });
-    }
-
-    return (
-        <div>
-            <h1>New message</h1>
-
-            <form onSubmit={handelOnSubmit}>
-                <div>
-                    Title:<input value={name} type="text" onChange={(e) => setTitle(e.target.value)}/>
-                </div>
-                <select onChange={(e)=>setUserTo(e)}>
-
-                    <option>Add user to chat</option>
-                    {users.map((option, index) => {
-                        return <option value={option.id_user} key={index}>
-                            {option.username}
-                        </option>
-                    })}
-                </select>
-                <br/>
-                <button className={"button"} onClick={()=>handelNavigate()}></button>
-                <br/>
-                    <!--add users here ? -->
-                <div>
-                    {emails.map((email,index)=>{
-
-                        return <div>
-                            email {index+1} :<input  value={email.email} type="text"
-                                                     onChange={(e) => handelSetEmail(e,index)}/>
-                        </div>
-                    })}
-                    <button onClick={(e)=>handelAddMail(e)}>add mail</button><button onClick={(e)=>handelRemoveEmail(e)}>remove mail</button>
-                </div>
-
-
-                <button>Update</button>
-            </form>
-
-        </div>
-    )
-}
 
 
 
@@ -99,7 +48,7 @@ function App() {
                     <Route path={"/"} element={<FrontPage users={users} setUserTo={setUserTo}/> }/>
                     <Route path={"/user/*"} element={<UserPage setActiveChat={setActiveChat} activeUserId={activeUserId} activeUserName={activeUserName}/>}/>
                     <Route path={"/edituser"} element={<EditUser activeUserId={activeUserId}/>}/>
-                    <Route path={"/newchat"} element={<NewChat users={users} activeUserId={activeUserId}/>}/>
+                    <Route path={"/newchat"} element={<NewChat setUserTo={setUserTo} users={users} activeUserId={activeUserId}/>}/>
 
                 </Routes>
             </HashRouter>
---
+++ src/main/client/src/newchat.jsx
@@ -0,0 +1,68 @@
+import {useEffect, useState} from "react";
+
+export function NewChat({users,activeUserId,setUserTo}) {
+    const [title, setTitle] = useState("");
+    const [chatters, setChatters] = useState([]);
+    const [chatter, setChatter] = useState("");
+
+
+    async function handelOnSubmit(){
+        await fetch("/api/chats", {
+            method: "POST",
+            body: JSON.stringify({title,chatters}),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+    }
+
+    function handelAddUser(e) {
+        e.preventDefault();
+        setChatters(prevState => [...prevState, chatter])
+    }
+
+    function setSelectedUser(e) {
+        e.preventDefault();
+        setChatter(e.target.value);
+
+
+
+    }
+
+    return (
+        <div>
+            <h1>New message</h1>
+
+            <form onSubmit={handelAddUser}>
+                <div>
+                    Title:<input value={title} type="text" onChange={(e) => setTitle(e.target.value)}/>
+                </div>
+                <select onChange={(e)=>setSelectedUser(e)}>
+
+                    <option>Add user to chat</option>
+                    {users.map((option, index) => {
+                        return <option value={option.id_user} key={index}>
+                            {option.username}
+                        </option>
+                    })}
+                </select>
+                <br/>
+                <button className={"button"} onClick={(e)=>handelAddUser(e)}>Add User</button>
+            </form>
+            <br/>
+
+            <div>
+                {chatters.map((id)=>{
+
+                    return <h1>{users[id-1].username}</h1>
+                })}
+
+            </div>
+
+            <button className={"button"} onClick={(e)=>handelOnSubmit(e)}>Add User</button>
+
+
+
+        </div>
+    )
+}
